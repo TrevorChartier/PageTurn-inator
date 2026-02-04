4cecf5fac738e5495ee80f5ea93abf2d73302267
TrevorChartier <chartier.trevor@gmail.com>
2026-02-04 13:50:40 -0700

    Add move_smooth Method to Control Servo Speed

--- src/servo.py
+++ src/servo.py
@@ -7,8 +7,22 @@ class Servo:
         kit = ServoKit(channels=16)
         self.__servo = kit.servo[pin]
         
+        self.__current_angle = 180
+        self.turn_right()
+        
     def turn_left(self):
-        self.__servo.angle = 1
+        self.__move_smooth(1, 0.6, 75)
         
     def turn_right(self):
-        self.__servo.angle = 180
+        self.__move_smooth(180, 0.6, 75)
+        
+    def __move_smooth(self, target_angle, duration=1.0, steps=50):
+        start_angle = self.__current_angle
+        step_angle = (target_angle - start_angle) / steps
+        step_time = duration / steps
+        
+        for i in range(steps):
+            self.__servo.angle = start_angle + (step_angle * i)
+            time.sleep(step_time)
+            
+        self.__current_angle = target_angle
